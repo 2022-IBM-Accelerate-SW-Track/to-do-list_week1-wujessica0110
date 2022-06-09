25fc0622c653e0ec684176b17338c7e82136c109
Jessica Wu <jw877@cornell.edu>
2022-06-08 17:21:48 -0700

    finished about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "./About.css";
-//import < profile_pic_name > from "../assets/profile_pic_name.png";
+import profile_pic from "../assets/profile_pic_name.png";
 
 export default class About extends Component {
   render() {
@@ -8,18 +8,19 @@ export default class About extends Component {
       <div>
         <div class="split left">
           <div className="centered">
-            <img
-              className="profile_image"
-              // Image goes here
-              alt="Profile Pic"
-            ></img>
+            <img src={profile_pic} />
           </div>
         </div>
         <div className="split right">
           <div className="centered">
             <div className="name_title">Jessica Wu</div>
             <div className="brief_description">
-              Hi! I am a rising junior CS major at Cornell University.
+              I am a rising junior at Cornell University who is majoring in CS
+              and triple minoring in Information Science: UX Design, Law,
+              and Business. In my free time I enjoy various forms of arts
+              and crafts such as crocheting, knitting, and embroidery and I also
+              really love to dance and play badminton. A fun fact about me is that
+              I have 4 cats!
 
             </div>
           </div>
