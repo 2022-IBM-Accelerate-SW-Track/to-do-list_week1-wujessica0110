bd87e8c3ed08510d27b0b1b9b262a989f7c0c82e
Jessica Wu <jw877@cornell.edu>
2022-06-08 17:00:42 -0700

    Add intro and name

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+//import < profile_pic_name > from "../assets/profile_pic_name.png";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              // Image goes here
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Jessica Wu</div>
+            <div className="brief_description">
+              Hi! I am a rising junior CS major at Cornell University.
+
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
